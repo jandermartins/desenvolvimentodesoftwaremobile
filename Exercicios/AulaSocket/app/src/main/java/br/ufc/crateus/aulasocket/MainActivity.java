32409f4679f638994e5a064c0a711a99daf4ca76
Jander <jandermartins@alu.ufc.br>
2019-09-17 14:05:35 -0300

    server

--- Exercicios/AulaSocket/app/src/main/java/br/ufc/crateus/aulasocket/MainActivity.java
+++ Exercicios/AulaSocket/app/src/main/java/br/ufc/crateus/aulasocket/MainActivity.java
@@ -2,11 +2,13 @@ package br.ufc.crateus.aulasocket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editText;
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         editText = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,12 +39,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         try {
-                            Socket socket = new Socket("192.168.0.114", 50000);
+                            Socket socket = new Socket("10.5.30.68", 5560);
 
                             DataInputStream dIn = new DataInputStream(socket.getInputStream());
                             DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
 
                             dOut.writeUTF(editText.getText().toString());
+
+                            String mensagem = dIn.readUTF();
+
+                            textView.setText(mensagem.toString());
 
                             dIn.close();
                             dOut.close();
---
+++ Exercicios/Java Server/Socket/src/Socket.java
@@ -0,0 +1,41 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Scanner;
+
+public class Socket {
+	private static ServerSocket server;
+
+	public static void main(String[] args) {
+		try {
+			server = new ServerSocket(5560);
+
+			while (true) {
+				java.net.Socket socket = server.accept();
+
+				DataInputStream dIn = new DataInputStream(socket.getInputStream());
+				DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+
+				System.out.println(dIn.readUTF());
+
+				
+				
+				Scanner scan = new Scanner(System.in);
+				
+				System.out.println("Digite a mensagem");
+				String msg = scan.next();
+				
+				dOut.writeUTF(msg);
+
+				dOut.flush();
+				dOut.close();
+				dIn.close();
+				socket.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
